38bc6f7f2ff040456ed04218f7bf3203d8df55b3
key5n <k.nogami.472@stn.nitech.ac.jp>
2022-10-07 04:00:29 +0900

    added explanation and example for my collabolator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import "../styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>名古屋工業大学工大祭アプリ</title>
+        <meta
+          name="description"
+          content="第60回名古屋工業大学工大祭の予約管理とアプリのです。スマホ版の使用を強くおすすめします。"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
