ff24b49eaea5a7f2a24e2202ec5d29490abd6ca5
key5n <k.nogami.472@stn.nitech.ac.jp>
2022-10-11 05:03:33 +0900

    added default exportation description

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import styles from "styles/components.module.css";
 import { LoginTemplate } from "components/templates/LoginTemplate";
 
-const Login: FC = () => {
+const Login = () => {
   return <LoginTemplate />;
 };
+
+export default Login;
