00b457e0ff50a00bf08e777691b860cac8bbb13a
key5n <k.nogami.472@stn.nitech.ac.jp>
2022-11-05 22:38:59 +0900

    applied for hosting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from "next";
-import Link from "next/link";
 import { IndexTemplate } from "components/templates/IndexTemplate";
 import { useUser } from "context/userContext";
 
 const Index: NextPage = () => {
-  const { user, loadingUser } = useUser();
-  return <>{loadingUser ? <p>loading</p> : <IndexTemplate />}</>;
+  // const { user, loadingUser } = useUser();
+  return (
+    // <>
+    //   {loadingUser ? (
+    //     <p>loading</p>
+    //   ) : (
+    //     // <IndexTemplate />
+    //     <p>a</p>
+    //   )}
+    // </>
+    <p>now developing this page</p>
+  );
 };
 
 export default Index;
