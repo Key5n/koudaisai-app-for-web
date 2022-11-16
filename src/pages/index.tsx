3168f457aa8b50fcb86a250fe09b9e9d2913fa8c
key5n <k.nogami.472@stn.nitech.ac.jp>
2022-11-17 01:13:55 +0900

    added Link so that can easily access to qr-scan

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from "next";
 import { IndexTemplate } from "components/templates/IndexTemplate";
 import { useUser } from "context/userContext";
+import Link from "next/link";
 
 const Index: NextPage = () => {
   const { user, loadingUser } = useUser();
-  return <>{loadingUser ? <p>loading</p> : ""}</>;
+  return (
+    <>
+      {loadingUser ? (
+        <p>loading</p>
+      ) : (
+        <Link href="/admin/qr-scan">qrコード読み取り</Link>
+      )}
+    </>
+  );
 };
 
 export default Index;
