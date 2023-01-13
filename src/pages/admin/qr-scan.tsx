48d00caf4f32a0f6d065c8d48e6c55cf3a8a44e1
key5n <k.nogami.472@stn.nitech.ac.jp>
2023-01-13 18:59:46 +0900

    deleted almost of all files

--- src/pages/admin/qr-scan.tsx
+++
@@ -1,7 +0,0 @@
-import { QRScanTemplate } from "components/templates/QRScanTemplate";
-
-const QRScan = () => {
-  return <QRScanTemplate />;
-};
-
-export default QRScan;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
-import type { NextPage } from "next";
-import { IndexTemplate } from "components/templates/IndexTemplate";
-import { useUser } from "context/userContext";
-import Link from "next/link";
-import { ConfigTemplate } from "components/templates/ConfigTemplate";
+// import type { NextPage } from "next";
+// import { IndexTemplate } from "components/templates/IndexTemplate";
+// import { useUser } from "context/userContext";
+// import Link from "next/link";
+// import { ConfigTemplate } from "components/templates/ConfigTemplate";
 
-const Index: NextPage = () => {
-  const { loadingUser } = useUser();
+const Index = () => {
+  // const { loadingUser } = useUser();
   return (
     <>
-      {loadingUser ? (
+      {/* {loadingUser ? (
         <p>loading</p>
       ) : (
         <ConfigTemplate />
-      )}
+      )} */}
+      <p>indexa</p>
     </>
   );
 };
