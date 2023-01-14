66026e25a95f5272518e7febba300cd925b7a1af
key5n <k.nogami.472@stn.nitech.ac.jp>
2023-01-15 00:44:31 +0900

    around signup

--- src/features/signup/signup.tsx
+++ src/features/signup/signup.tsx
@@ -1,11 +1,13 @@
 import styles from "./styles.module.css";
 import clsx from "clsx";
 import { KofunmanTalking } from "../ui/KofunmanTalking";
+import { SignupForm } from "./signup-form";
 
 export const Signup = () => {
   return (
     <main className={styles.module}>
       <KofunmanTalking line={{ children: "予約のために次のことを教えてね" }} />
+      <SignupForm />
     </main>
   );
 };
