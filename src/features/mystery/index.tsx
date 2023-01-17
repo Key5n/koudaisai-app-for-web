0fc09369da5e719b872881c524ed4ce7868bb269
key5n <k.nogami.472@stn.nitech.ac.jp>
2023-01-17 21:03:40 +0900

    modified

--- src/features/mystery/index.tsx
+++ src/features/mystery/index.tsx
@@ -1,1 +1,2 @@
-export * from "./mystery"
+export * from "./mystery";
+export * from "./doughnutChart";
--- src/features/mystery/mystery.tsx
+++ src/features/mystery/mystery.tsx
@@ -1,3 +1,40 @@
-export function mystery() {
-  return <p>mystery</p>;
-}
+import { useState } from "react";
+import { DoughnutChart } from "./doughnutChart";
+import { Field } from "./Field";
+import styles from "./styles.module.css";
+
+type Props = {
+  data: { numOfChallenger: number; numOfSolver: number };
+};
+
+type mysteryData = {
+  numOfChallenger: number;
+  numOfSolver: number;
+  numOfNewChallenger: number;
+  numOfNewSolver: number;
+};
+
+export const Mystery = ({ data }: Props) => {
+  const [mysteryData, setMysteryData] = useState<mysteryData>({
+    numOfChallenger: data.numOfChallenger,
+    numOfSolver: data.numOfSolver,
+    numOfNewChallenger: 0,
+    numOfNewSolver: 0,
+  });
+
+  return (
+    <div className={styles.module}>
+      <DoughnutChart
+        value={[
+          mysteryData.numOfSolver,
+          mysteryData.numOfChallenger - mysteryData.numOfSolver,
+        ]}
+      />
+      <div className={styles.data}>
+        <Field title="挑戦人数" value={mysteryData.numOfChallenger} />
+        <Field title="完全クリア人数" value={mysteryData.numOfSolver} />
+      </div>
+      {/* <MysteryForm mysteryData={mysteryData} setMysteryData={setMysteryData} /> */}
+    </div>
+  );
+};
