6d8fb106010d25585f3d4863bdcad4bfa5314fae
Key5n <k.nogami.472@stn.nitech.ac.jp>
2023-04-20 18:38:50 +0900

    fix: rename qr-scan to qr-scanner

--- src/pages/admin/mystery/index.tsx
+++ src/pages/admin/mystery/index.tsx
@@ -1,5 +1,5 @@
 import { createGetLayoutWithFooter } from "@/features/layouts/LayoutWithFooter";
-import { QRScanner } from "@/features/qr-scan";
+import { QRScanner } from "@/features/qr-scanner";
 import { NextPageWithLayout } from "@/lib/next/types";
 
 const Page: NextPageWithLayout = () => {
