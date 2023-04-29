519bbcb0c654206b6f7ebb2df1fa4d6bba3c9d9a
Key5n <k.nogami.472@stn.nitech.ac.jp>
2023-04-30 06:41:21 +0900

    fix: set home UI unmanipulatable

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -1,20 +1,32 @@
 import styles from "./styles.module.css";
-import clsx from "clsx";
 import { KofunmanTalking } from "../ui/KofunmanTalking";
 import Link from "next/link";
+import { useAppDispatch } from "@/lib/reduxHooks";
+import { addNotification } from "../ui/Notification/notificationSlice";
+import { useEffect } from "react";
 export const Home = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(
+      addNotification({
+        type: "info",
+        title: "お知らせ",
+        description: "現在予約不可です。",
+      })
+    );
+  }, []);
   return (
     <main className={styles.module}>
       <KofunmanTalking
         line={{ children: "僕は古墳マン。\n工大祭の案内をするよ！" }}
         mode={0}
       />
-      <Link href="signup">
+      <Link href="">
         <a className={styles.signupButton}>新規予約</a>
       </Link>
       <div className={styles.login}>
         <p>すでに工大祭に予約された場合</p>
-        <Link href="login">
+        <Link href="">
           <a className={styles.loginButton}>ログイン</a>
         </Link>
       </div>
