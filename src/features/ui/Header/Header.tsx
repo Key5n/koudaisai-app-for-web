c0bb4a37415bf55da97ba52d92aa5b3327a2d59b
Key5n <k.nogami.472@stn.nitech.ac.jp>
2023-04-30 23:16:01 +0900

    fix: add icon to header

--- src/features/ui/Header/Header.tsx
+++ src/features/ui/Header/Header.tsx
@@ -2,6 +2,8 @@ import styles from "./styles.module.css";
 import clsx from "clsx";
 import { ComponentPropsWithoutRef } from "react";
 import { Hamburger } from "./Hamburger";
+import Image from "next/image";
+import koudaisaiIcon from "@/assets/daisai.svg";
 
 type Props = {
   className?: string;
@@ -10,6 +12,12 @@ export const Header = ({ className, children, ...props }: Props) => {
   return (
     <>
       <header {...props} className={clsx(styles.module, className)}>
+        <Image
+          src={koudaisaiIcon}
+          width="40px"
+          height="40px"
+          alt="工大祭アイコン"
+        />
         {children}
         <Hamburger />
       </header>
